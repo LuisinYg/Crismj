dbe682998055d745deca236d7a20b981d12b0520
LuisinYg <luisperez.lpz719@gmail.com>
2025-04-03 23:26:37 -0600

    Initial commit

---
+++ Downloads/nopongascusa/nopongascusa/src/main/java/com/example/nopongascusa/NopongascusaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nopongascusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NopongascusaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NopongascusaApplication.class, args);
+	}
+
+}
